52bf4e93df5932aa72258defd264cfdb75904552
root <workpraytoday@gmail.com>
2018-11-19 16:35:04 -0600

    Works... May eed to change field of view

--- vision.py
+++ vision.py
@@ -4,24 +4,32 @@ import cv2
 import numpy as np
 import math
 cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_EXPOSURE, -1)
+
 #Calculated by hand
-ANGLE_OF_LAUNCH = 45
-GRAVITY_INCHES = 386
 HFOV = 55.794542
 VFOV = 50.92
-RADIUS_OF_FLYWHEEL = 4
 #Calculated in program
 H_FOCAL_LENGTH = 808
 V_FOCAL_LENGTH = 707
 
+def make_720p():
+    cap.set(3, 1280)
+    cap.set(4, 720)
 
-#camera_matrix =np.ndarray((3,3), buffer=np.array([[517.2110395316563, 0.0, 334.3418325883858], [0.0, 543.2949569504035, 211.75050568262293], [0.0, 0.0, 1.0]])) # offset = 1*itemsize, i.e. skip first element
-#dist =np.ndarray((1,5), buffer=np.array([[0.07251015712570133, -0.7601727484403693, -0.02290637861280213, 0.01562729685105215, 2.5647585221601936]])) # offset = 1*itemsize, i.e. skip first element
+def make_480p():
+    cap.set(3, 640)
+    cap.set(4, 480)
 
+def change_res(width, height):
+    cap.set(3, width)
+    cap.set(4, height)
 
+change_res(640, 480)
 # Masks the video based on a range of hsv colors
 # Takes in a frame, returns a masked frame
 def threshold_video(frame):
+
     #Gets the shape of video
     screenHeight, screenWidth, channels = frame.shape
     #Gets center of height and width
@@ -31,9 +39,9 @@ def threshold_video(frame):
 
     # Convert BGR to HSV
     hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
-    # define range of blue (my current shirt) in HSV
-    lower_color = np.array([0, 126, 76])
-    upper_color = np.array([180, 255, 255])
+    # define range of blue (my notebook) in HSV
+    lower_color = np.array([0, 135, 53])
+    upper_color = np.array([42, 218, 255])
     # hold the HSV image to get only red colors
     mask = cv2.inRange(hsv, lower_color, upper_color)
     # Shows the theshold image in a new window
@@ -49,7 +57,7 @@ def findContours(cap, mask):
     # Take each frame
     _, frame = cap.read()
     #Flips the frame so my right is the image's right (probably need to change this
-    frame = cv2.flip(frame, 1)
+    #frame = cv2.flip(frame, 1)
     #Gets the shape of video
     screenHeight, screenWidth, channels = frame.shape
     #Gets center of height and width
@@ -57,11 +65,95 @@ def findContours(cap, mask):
     centerY = (screenHeight / 2) - .5
     #Copies frame and stores it in image
     image = frame.copy()
-    #Processes the contours, takes in (contours, output_image, (centerOfImage)
-    processContours(contours, image, centerX, centerY)
+    #Processes the contours, takes in (contours, output_image, (centerOfImage) #TODO finding largest
+    if len(contours) != 0:
+        processLargestContour(contours, image, centerX, centerY)
     #Shows the contours overlayed on the original video
     cv2.imshow("Contours", image)
 
+#Draws and calculates properties of largest contour
+def processLargestContour(contours, image, centerX, centerY):
+    if len(contours) != 0:
+
+        cnt = findBiggestContours(contours)
+        #Get moments of contour; mainly for centroid
+        M = cv2.moments(cnt)
+        #Get convex hull (bounding polygon on contour)
+        hull = cv2.convexHull(cnt)
+        #Calculate Contour area
+        cntArea = cv2.contourArea(cnt)
+        #calculate area of convex hull
+        hullArea = cv2.contourArea(hull)
+        #Filters contours based off of size
+        if (checkContours(cntArea, hullArea)):
+            #Gets the centeroids of contour
+            if M["m00"] != 0:
+                cx = int(M["m10"] / M["m00"])
+                cy = int(M["m01"] / M["m00"])
+            else:
+                cx, cy = 0, 0
+
+            #Gets rotated bounding rectangle of contour
+            rect = cv2.minAreaRect(cnt)
+            #Creates box around that rectangle
+            box = cv2.boxPoints(rect)
+            #Not exactly sure
+            box = np.int0(box)
+            #Gets center of rotated rectangle
+            center = rect[0]
+            #Gets rotation of rectangle; same as rotation of contour
+            rotation = rect[2]
+            #Gets width and height of rotated rectangle
+            width = rect[1][0]
+            height = rect[1][1]
+            #Maps rotation to (-90 to 90). Makes it easier to tell direction of slant
+            rotation = translateRotation(rotation, width, height)
+            #Gets smaller side
+            if width > height:
+                smaller_side = height
+            else:
+                smaller_side = width
+            #Calculates yaw of contour (horizontal position in degrees)
+            yaw = calculateYaw(cx, centerX, H_FOCAL_LENGTH)
+            #Calculates yaw of contour (horizontal position in degrees)
+            pitch = calculatePitch(cy, centerY, V_FOCAL_LENGTH)
+            #
+            #Adds padding for text
+            padding  = -8 - math.ceil(.5*smaller_side)
+            #Draws rotated rectangle
+            cv2.drawContours(image, [box], 0, (23, 184, 80), 3)
+
+            #Draws a vertical white line passing through center of contour
+            cv2.line(image, (cx, screenHeight), (cx, 0), (255, 255, 255))
+            #Draws a white circle at center of contour
+            cv2.circle(image, (cx, cy), 6, (255, 255, 255))
+            #Puts the rotation on screen
+            cv2.putText(image, "Rotation: " + str(rotation), (cx + 40, cy + padding), cv2.FONT_HERSHEY_COMPLEX, .6, (255, 255, 255))
+            #Puts the yaw on screen
+            cv2.putText(image, "Yaw: " + str(yaw), (cx+ 40, cy + padding -16), cv2.FONT_HERSHEY_COMPLEX, .6, (255, 255, 255))
+            #Puts the Pitch on screen
+            cv2.putText(image, "Pitch: " + str(pitch), (cx+ 80, cy + padding -42), cv2.FONT_HERSHEY_COMPLEX, .6, (255, 255, 255))
+
+
+            #Draws the convex hull
+            #cv2.drawContours(image, [hull], 0, (23, 184, 80), 3)
+            #Draws the contours
+            cv2.drawContours(image, [cnt], 0, (23, 184, 80), 1)
+
+            #Gets the (x, y) and radius of the enclosing circle of contour
+            (x, y), radius = cv2.minEnclosingCircle(cnt)
+            #Rounds center of enclosing circle
+            center = (int(x), int(y))
+            #Rounds radius of enclosning circle
+            radius = int(radius)
+            #Makes bounding rectangle of contour
+            rx, ry, rw, rh = cv2.boundingRect(cnt)
+
+            #Draws countour of bounding rectangle and enclosing circle in green
+            cv2.rectangle(image, (rx, ry), (rx + rw, ry + rh), (23, 184, 80), 1)
+            cv2.circle(image, center, radius, (23, 184, 80), 1)
+
+            sendImportantContourInfo(cx, cy, yaw, pitch, rotation, cntArea);
 #Draws and calculates contours and their properties
 def processContours(contours, image, centerX, centerY):
 
@@ -112,9 +204,8 @@ def processContours(contours, image, centerX, centerY):
             #Adds padding for text
             padding  = -8 - math.ceil(.5*smaller_side)
             #Draws rotated rectangle
-            #cv2.drawContours(image, [box], 0, (23, 184, 80), 3)
-            distance = calculateDistance(10.25, 8.5, pitch);
-            velocity = calculateShooterVelocity(distance)
+            cv2.drawContours(image, [box], 0, (23, 184, 80), 3)
+
             #Draws a vertical white line passing through center of contour
             cv2.line(image, (cx, screenHeight), (cx, 0), (255, 255, 255))
             #Draws a white circle at center of contour
@@ -125,8 +216,7 @@ def processContours(contours, image, centerX, centerY):
             cv2.putText(image, "Yaw: " + str(yaw), (cx+ 40, cy + padding -16), cv2.FONT_HERSHEY_COMPLEX, .6, (255, 255, 255))
             #Puts the Pitch on screen
             cv2.putText(image, "Pitch: " + str(pitch), (cx+ 80, cy + padding -42), cv2.FONT_HERSHEY_COMPLEX, .6, (255, 255, 255))
-            cv2.putText(image, "Distance: " + str(distance), (cx+ 80, cy + padding -60), cv2.FONT_HERSHEY_COMPLEX, .6, (255, 255, 255))
-            cv2.putText(image, "Velocity: " + str(velocity), (cx+ 80, cy + padding -84), cv2.FONT_HERSHEY_COMPLEX, .6, (255, 255, 255))
+
 
             #Draws the convex hull
             #cv2.drawContours(image, [hull], 0, (23, 184, 80), 3)
@@ -146,7 +236,14 @@ def processContours(contours, image, centerX, centerY):
             cv2.rectangle(image, (rx, ry), (rx + rw, ry + rh), (23, 184, 80), 1)
             cv2.circle(image, center, radius, (23, 184, 80), 1)
 
+            sendImportantContourInfo(cx, cy, yaw, pitch, rotation, cntArea);
 
+def findBiggestContours(contours):
+    if len(contours) != 0:
+        c = max(contours, key = cv2.contourArea)
+        return c
+    else:
+        return None;
 
 #Draws contours on blank, black image
 def findContoursNewImage(cap, mask, newImage):
@@ -155,7 +252,7 @@ def findContoursNewImage(cap, mask, newImage):
     # Take each frame
     _, frame = cap.read()
     # Flips the frame so my right is the image's right (probably need to change this
-    frame = cv2.flip(frame, 1)
+    #frame = cv2.flip(frame, 1)
     # Gets the shape of video
     screenHeight, screenWidth, channels = frame.shape
     # Gets center of height and width
@@ -164,13 +261,13 @@ def findContoursNewImage(cap, mask, newImage):
     #makes image equal to the assigned image
     image = newImage
     #Draws contours and other stuff
-    processContours(contours, image, centerX, centerY)
+    processLargestContour(contours, image, centerX, centerY)
     #Draws on a blank image
     cv2.imshow("Contours New", image)
 
 #Checks if contours are worthy based off of contour area and (not currently) hull area
 def checkContours(cntSize, hullSize):
-    if(cntSize > 1000):
+    if(cntSize > 10000):
         return True
     else:
         return False;
@@ -185,9 +282,25 @@ def translateRotation(rotation, width, height):
     return rotation
 def calculateDistance(heightOfCamera, heightOfTarget, pitch):
     heightOfCameraFromTarget = heightOfTarget - heightOfCamera
-    if heightOfCameraFromTarget < 0:
-        heightOfCameraFromTarget
+
     #Uses trig and pitch to find distance to target
+    '''
+    d = distance
+    h = height between camera and target
+    a = angle/pitch
+    
+    tan a = h/d (opposite over adjacent)
+    
+    d = h / tan a
+    
+                         .                 
+                        /|        
+                       / |       
+                      /  |h        
+                     /a  |       
+              camera -----
+                       d         
+    '''
     distance = math.fabs(heightOfCameraFromTarget / math.tan(math.radians(pitch)))
 
     return distance
@@ -202,19 +315,22 @@ def calculatePitch(pixelY, centerY, vFocalLength):
     #Just stopped working have to do this:
     pitch *= -1
     return pitch
-def calculateShooterVelocity(distance):
-    linear_velocity = math.fabs(math.sqrt((distance * GRAVITY_INCHES) / math.sin(math.radians(2 * ANGLE_OF_LAUNCH))))
-    print("Linear Velocity: " + str(linear_velocity))
-    angular_velocity = (linear_velocity / RADIUS_OF_FLYWHEEL)
-    revolution = 2 * math.pi
-    rpm = angular_velocity * (60 / revolution)
-    print("RPM: " + str(rpm))
+def sendImportantContourInfo(contourX, contourY, contourYaw, contourPitch, contourRotation, contourArea):
+    print("Contour X: " + str(contourX))
+    print("Contour Y: " + str(contourY))
+    print("Contour Yaw: " + str(contourYaw))
+    print("Contour Pitch: " + str(contourPitch))
+    print("Contour Rotation: " + str(contourRotation))
+    print("Contour Area: " + str(contourArea))
 
-    return rpm
+
+
+
+
 while(True):
 
     _, frame = cap.read()
-    frame = cv2.flip(frame, 1)
+    #frame = cv2.flip(frame, 1)
     screenHeight, screenWidth, channels = frame.shape
     print("Screen width" + str(screenWidth))
     #Calculates focal Length-
